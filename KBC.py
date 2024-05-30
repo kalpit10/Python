62489645d6345cc3aba3e464578ade2d2f2386b6
Kalpit Swami <72384932+kalpit10@users.noreply.github.com>
2024-05-30 09:42:39 +0000

    Changed Code

--- KBC.py
+++ KBC.py
@@ -1,30 +1,29 @@
-def KBC(question, answer, amount):
-  print(question)
-  user_answer = input("Enter your answer: ")
-  if user_answer == answer:
-      print("Correct answer!")
-      amount += 1000
-      print("You won", amount)
-      return True, amount
-  else:
-      print("Wrong answer!")
-      return False, amount
+def KBC(questions):
+    amount = 0
+    for qa in questions:
+        print(qa["question"])
+        user_answer = input("Enter your answer: ")
+        if user_answer == qa["answer"]:
+            print("Correct answer!")
+            amount += 1000
+            print("You won", amount)
+        else:
+            print("Wrong answer!")
+            break
+    print("Game over! Your total winnings are:", amount)
+
 
 # List of question-answer pairs
-questions = [
-  {"question": "What is the capital of INDIA?", "answer": "New Delhi"},
-  {"question": "How many states are there in INDIA?", "answer": "28"},
-  {"question": "Who is the prime minister of INDIA?", "answer": "Narendra Modi"}
-]
+questions = [{
+    "question": "What is the capital of INDIA?",
+    "answer": "New Delhi"
+}, {
+    "question": "How many states are there in INDIA?",
+    "answer": "28"
+}, {
+    "question": "Who is the prime minister of INDIA?",
+    "answer": "Narendra Modi"
+}]
 
-# Initial amount
-amount = 0
-
-# Iterate over each question-answer pair
-for qa in questions:
-  #correct is a boolean
-  correct, amount = KBC(qa["question"], qa["answer"], amount)
-  if not correct:
-      break
-
-print("Game over! Your total winnings are:", amount)
+# Run the game
+KBC(questions)
